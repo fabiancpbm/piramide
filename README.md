d1dc141bfec255e85190967156f2f54235962770
Fabian <fabiancpbm@gmail.com>
2021-05-22 23:27:05 -0300

    Adicionando funcionalidade de tronco de pirâmide.

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
-# piramide
-## Para executar:
-- Execute o comando "python3 piramide.py" ou "python3 piramide.py h", onde h é a altura da pirâmide.
+# piramide com corte
+
+## Para executar a pirâmide:
+
+- Execute o comando "python3 piramide.py" ou "python3 piramide.py height", onde height é a altura da pirâmide.
+
+## Para executar a pirâmide com corte:
+
+- Execute o comando "python3 piramide.py height cutPercentage", onde height é a altura da pirâmide e cutPercentage é a porcentagem que se deseja cortar a pirâmide (valor entre 0 e 1).
--- piramide.py
+++ piramide.py
@@ -1,45 +1,72 @@
 from OpenGL.GLUT import *
 from OpenGL.GLU import *
 from OpenGL.GL import *
+import math
 
-h = int (sys.argv[1]) if len(sys.argv) > 1 else 1.7
+# Altura do  triângulo.
+height = int (sys.argv[1]) if len(sys.argv) > 1 else 1.7
+
+# Porcentagem de corte da altura (valor de 0 a 1). 0 significa 0% de corte, 1 significa 100% de corte.
+cutPercentage = float (sys.argv[2]) if len(sys.argv) > 2 else 0
+
+# Altura cortada em cutPercentage porcento.
+cutH = height * (cutPercentage)
+
+# Altura restante pós corte.
+h = height * (1 - cutPercentage)
+
+# Ângulo do triângulo
+theta = math.pi/6
+
+# Hipotenusa do triângulo cujo cateto oposto é cutH e ângulo theta.
+hip = cutH/math.sin(theta)
+
+# O valor de corte é o cateto adjacente do triângulo cujo cateto oposto é cutH e ângulo theta.
+cutValue = (hip**2 - cutH**2)**0.5
 
 vertices = (
-    ( 0, h, 0),
-    ( 1, 0, 1),
-    ( 1, 0,-1),
-    (-1, 0,-1),
-    (-1, 0, 1)
+    ( 1,-1,-1),
+    ( cutValue, h,-cutValue),
+    (-cutValue, h,-cutValue),
+    (-1,-1,-1),
+    ( 1,-1, 1),
+    ( cutValue, h, cutValue),
+    (-1,-1, 1),
+    (-cutValue, h, cutValue),
     )
 
 linhas = (
     (0,1),
-    (0,2),
     (0,3),
     (0,4),
-    (1,2),
+    (2,1),
     (2,3),
-    (3,4),
-    (4,1)
+    (2,7),
+    (6,3),
+    (6,4),
+    (6,7),
+    (5,1),
+    (5,4),
+    (5,7),
     )
 
 faces = (
-    (0,1,2),
-    (0,2,3),
-    (0,3,4),
-    (0,4,1),
-    (1,2,3),
-    (1,3,4)
+    (0,1,2,3),
+    (3,2,7,6),
+    (6,7,5,4),
+    (4,5,1,0),
+    (1,5,7,2),
+    (4,0,3,6)
     )
 
-cores = ( (1,0,0),(1,1,0),(0,1,0),(0,1,1),(0,0,1),(0,0,1) )
-
+cores = ( (1,0,0),(1,1,0),(0,1,0),(0,1,1),(0,0,1),(1,0,1),(0.5,1,1),(1,0,0.5) )
 def Piramide():
-    glBegin(GL_TRIANGLES)
+    glBegin(GL_QUADS)
     i = 0
     for face in faces:
         glColor3fv(cores[i])
         for vertex in face:
+            glColor3fv(cores[vertex])
             glVertex3fv(vertices[vertex])
         i = i+1
     glEnd()
